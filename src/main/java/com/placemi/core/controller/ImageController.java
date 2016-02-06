7be8d53f118246bbb76e7fca40f2c5a8c9a733e1
Brenden Palmer <brendenpalmer@gmail.com>
2016-02-06 01:35:50 -0800

    placemi, initial commit

---
+++ src/main/java/com/placemi/core/controller/ImageController.java
@@ -0,0 +1,23 @@
+package com.placemi.core;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by brendenpalmer on 20160206.
+ */
+
+@RestController
+public class ImageController {
+    @RequestMapping(value="/{width}x{height}", method= RequestMethod.GET)
+    public String image() {
+        return "Test";
+    }
+
+    @RequestMapping(value="/g/{width}x{height}", method= RequestMethod.GET)
+    public String grayscaleImage() {
+        return "Grayscale test";
+    }
+}
